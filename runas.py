35574ea6ea2c4903af260d5a8757231ca8f02c1e
Walentiny, Patrick <Patrick.Walentiny@Pearson.com>
2019-01-19 03:50:29 +0000

    First working prototype

--- runas.py
+++ runas.py
@@ -2,13 +2,10 @@
 
 import argparse
 import boto3
-import dateutil.parser
 import datetime
 import os
-import pytz
-import sqlite3
-import sys
-import yaml
+import pickle
+import subprocess
 
 DURATION = 900
 SERIAL_NUMBER = 'arn:aws:iam::056952386373:mfa/patrick.walentiny@pearson.com'
@@ -20,52 +17,53 @@ class CacheFileCorrupt(Exception):
     pass
 
 
-def assume_role(role):
+def assume_role(account_id, role):
     session_token = get_session_token()
-    print(session_token)
-    client = boto3.client('sts')
-    account_id =client.get_caller_identity().get('Account')
-    response = client.assume_role(
-        RoleArn='arn:aws:iam::{}:role/{}'.format(account_id, role),
-        RoleSessionName='PCS-Pipeline',
+    client = boto3.client(
+        'sts',
         aws_access_key_id=session_token['AccessKeyId'],
         aws_secret_access_key=session_token['SecretAccessKey'],
         aws_session_token=session_token['SessionToken']
+    )
+    response = client.assume_role(
+        RoleArn='arn:aws:iam::{}:role/{}'.format(account_id, role),
+        RoleSessionName='PCS-Pipeline',
     )
     config_session_environment(response['Credentials'])
 
 
 def config_session_environment(credentials):
     os.environ['AWS_ACCESS_KEY_ID'] = credentials['AccessKeyId']
-    os.environ['AWS_SECRET_KEY'] = credentials['SecretAccessKey']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = credentials['SecretAccessKey']
     os.environ['AWS_SESSION_TOKEN'] = credentials['SessionToken']
     
-
-def fix_dates(cache_data):
-    for session in cache_data:
-        cache_data[session]['Expiration'] = dateutil.parser.parse(cache_data[session]['Expiration']).replace(tzinfo=pytz.UTC)
-
 
 def get_args():
     # Parse the arguments
     parser = argparse.ArgumentParser(description='Deploy an applications cloudformation template.')
     parser.add_argument('--profile', help='Set the AWS profile in ~/.aws/configure to use', required=True)
+    parser.add_argument('--region', help='Set the region to use', required=True)
+    parser.add_argument('--account-id', help='Set the account id to use', required=True)
+    parser.add_argument('--role', help='Set the role to use', required=True)
+    parser.add_argument('command', nargs='*', help='The command to run')
     return parser.parse_args()
 
 
 def get_cache_data():
     # Open the cache file and see if there is an existing session we can use.
     try:
-        with open(cache_file, 'r') as cachefp:
-            cache_data = yaml.load(cachefp)
-            fix_dates(cache_data)
+        with open(cache_file, 'rb') as cachefp:
+            cache_data = pickle.load(cachefp)
             if not isinstance(cache_data, dict):
                 raise CacheFileCorrupt('The top level object of {} needs to be a dictionary.')
     except FileNotFoundError:
         # We need to seed the cache file with an empty dictionary.
-        os.mkdir(config_dir)
-        with open(cache_file, 'w') as cachefp:
-            yaml.dump({}, cachefp, default_flow_style=False)
+        try:
+            os.mkdir(config_dir)
+        except FileExistsError:
+            pass
+        with open(cache_file, 'wb') as cachefp:
+            pickle.dump({}, cachefp)
         cache_data = {}
     return cache_data
 
@@ -77,38 +75,35 @@ def get_session_token():
     # Check if we have an existing session, if so use it
     try:
         cached_session = cache_data[os.environ['AWS_PROFILE']]
-        print(cached_session['Expiration'])
-        if cached_session['Expiration'] > datetime.datetime.now():
+        if cached_session['Expiration'].replace(tzinfo=None) > datetime.datetime.now():
             return cached_session
-
     except KeyError:
-        # If not, Create one
-        token_code = input('MFA Code for {}: '.format(SERIAL_NUMBER))
-        client = boto3.client('sts')
-        response = client.get_session_token(
-            DurationSeconds=DURATION,
-            SerialNumber=SERIAL_NUMBER,
-            TokenCode=token_code
-        )
+        pass
 
-        # Write the created session to cache
-        cache_data = get_cache_data()
-        cache_data[os.environ['AWS_PROFILE']] = response['Credentials']
-        write_cache_data(cache_data)
-        return response['Credentials']
+    # If not, Create one
+    token_code = input('MFA Code for {}: '.format(SERIAL_NUMBER))
+    client = boto3.client('sts')
+    response = client.get_session_token(
+        DurationSeconds=DURATION,
+        SerialNumber=SERIAL_NUMBER,
+        TokenCode=token_code
+    )
+    # Write the created session to cache
+    cache_data = get_cache_data()
+    cache_data[os.environ['AWS_PROFILE']] = response['Credentials']
+    write_cache_data(cache_data)
+    return response['Credentials']
 
 
 def write_cache_data(cache_data):
-    cache_copy = dict(cache_data)
-    for session in cache_copy:
-        cache_copy[session]['Expiration'] = cache_copy[session]['Expiration'].replace(tzinfo=pytz.UTC).isoformat()
-    with open(cache_file, 'w') as cachefp:
-        yaml.dump(cache_copy, cachefp)
-        
+    with open(cache_file, 'wb') as cachefp:
+        pickle.dump(cache_data, cachefp)
 
 
 if __name__ == '__main__':
     args = get_args()
     os.environ['AWS_PROFILE'] = args.profile
+    os.environ['AWS_DEFAULT_REGION'] = args.region
     get_session_token()
-    assume_role('PowerUser')
+    assume_role(args.account_id, args.role)
+    subprocess.call(args.command)
