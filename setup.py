8c0639da2f88cdb1feb129009723730a02533f02
Walentiny, Patrick <Patrick.Walentiny@Pearson.com>
2019-01-24 18:48:05 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='runas',
+      version='1.0',
+      description='A runas utility for assuming roles in aws.',
+      author='Patrick Walentiny',
+      author_email='Patrick Walentiny',
+      url='https://github.com/pjwalen/runas',
+      scripts=['runas'],
+     )
