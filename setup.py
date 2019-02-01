5c057d0a5fa696efb9aef217c11bd31878282e11
Walentiny, Patrick <Patrick.Walentiny@Pearson.com>
2019-02-01 02:03:51 -0600

    configure pbr

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='runas',
-      version='1.0',
-      description='A runas utility for assuming roles in aws.',
-      author='Patrick Walentiny',
-      author_email='Patrick Walentiny',
-      url='https://github.com/pjwalen/runas',
-      scripts=['runas'],
-     )
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
